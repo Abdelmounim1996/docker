6e66c4f8a34e6f0a5867a1c1af41cbbf786b5fe8
ABDELMOUNIM ELKORCHI <74151267+Abdelmounim1996@users.noreply.github.com>
2023-11-04 03:31:16 +0100

    Update MPI_cluster.sh

--- MPI_cluster.sh
+++ MPI_cluster.sh
@@ -1,1 +1,6 @@
+docker search openmpi
+docker network create cluster-network
+docker run -it --name=master --network=cluster-network mfisherman/openmpi 
+docker run -it --name=worker1 --network=cluster-network mfisherman/openmpi 
+docker run -it --name=worker2 --network=cluster-network mfisherman/openmpi 
 
